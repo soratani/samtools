b38e1c017203d2c88d92262320e444753e61c0f8
liuwei <liuwei@zhihe.com>
2024-11-21 10:00:57 +0800

    feature: 权限控制

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,5 @@ export * as log from "./log";
 export * as color from "./color";
 export * as query from "./query";
 export * as transform from "./transform";
+export * from './permission';
 export { default as Graph } from "./graph";
