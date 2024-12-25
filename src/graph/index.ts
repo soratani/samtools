c6e4673ea246d8f2f54e685d60157a6cf4863062
liuwei <liuwei@zhihe.com>
2024-12-25 09:37:53 +0800

    wip: 图

--- src/graph/index.ts
+++ src/graph/index.ts
@@ -12,19 +12,19 @@ export default class Graph {
   }
 
   public addEdge(target: GraphId, source: GraphId, bothway = false) {
-    const targetMap = this.vertices.get(target);
+    const targetMap = this.vertices.get(target) || [];
     if (!targetMap.includes(source)) targetMap.push(source);
     if (bothway) {
-      const sourceMap = this.vertices.get(source);
+      const sourceMap = this.vertices.get(source) || [];
       if (!sourceMap.includes(target)) sourceMap.push(target);
     }
   }
 
   public path(target: GraphId, source: GraphId) {
     const distances: Record<GraphId, number> = {};
-    const previous: Record<GraphId, GraphId> = {};
+    const previous: Record<GraphId, GraphId | null> = {};
     const queue: GraphId[] = [];
-    const visited: Record<GraphId, boolean> = {};
+    const visited: Record<GraphId, boolean | null> = {};
     this.vertices.forEach((_val, key) => {
       distances[key] = Infinity;
       previous[key] = null;
@@ -34,7 +34,7 @@ export default class Graph {
     while (queue.length > 0) {
       queue.sort((a, b) => distances[a] - distances[b]);
       const closest = queue.shift();
-      if (visited[closest]) continue;
+      if (!closest || visited[closest]) continue;
       visited[closest] = true;
       const neighbors = this.vertices.get(closest);
       for (const neighbor in neighbors) {
@@ -47,11 +47,11 @@ export default class Graph {
         }
       }
     }
-    const path = [];
+    const path:GraphId[] = [];
     let shortestPathEnd = source;
     while (previous[shortestPathEnd]) {
       path.push(shortestPathEnd);
-      shortestPathEnd = previous[shortestPathEnd];
+      shortestPathEnd = previous[shortestPathEnd] as any;
     }
     path.push(target);
     path.reverse();
@@ -62,25 +62,3 @@ export default class Graph {
     };
   }
 }
-
-const graph = new Graph();
-graph.addVertex(1);
-graph.addVertex(2);
-graph.addVertex(3);
-graph.addVertex(4);
-graph.addVertex(5);
-graph.addVertex(6);
-graph.addVertex(7);
-graph.addVertex(8);
-graph.addVertex(9);
-graph.addEdge(1, 2);
-graph.addEdge(1, 3);
-graph.addEdge(1, 4);
-graph.addEdge(2, 4);
-graph.addEdge(2, 3);
-graph.addEdge(3, 7);
-graph.addEdge(4, 7);
-graph.addEdge(7, 9);
-graph.addEdge(2, 9);
-const a = graph.path(2, 9);
-console.log(a);
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,12 @@
-export * from "./interface";
-export * from "./type";
-export * from "./delay";
-export * from "./time";
 export * as tree from './tree';
 export * as set from "./set";
 export * as log from "./log";
 export * as color from "./color";
 export * as query from "./query";
 export * as transform from "./transform";
+export * from "./interface";
+export * from "./type";
+export * from "./delay";
+export * from "./time";
 export * from './permission';
 export { default as Graph } from "./graph";
